a3666fd2526c15cef033c122f1fb54ebe46e7ace
Yuta Mitsukawa <221205163@ccmailg.meijo-u.ac.jp>
2023-09-04 15:19:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/meijo/android/s221205163/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.meijo.android.s221205163;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
