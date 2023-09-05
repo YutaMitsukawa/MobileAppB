4d04418f4632564cd4d8a743f97a887ef80b8716
Yuta Mitsukawa <221205163@ccmailg.meijo-u.ac.jp>
2023-09-05 13:40:02 +0900

    テキストエディタの入力まで変更 画像ファイルを追加

--- app/src/main/java/jp/ac/meijo/android/s221205163/MainActivity.java
+++ app/src/main/java/jp/ac/meijo/android/s221205163/MainActivity.java
@@ -3,12 +3,45 @@ package jp.ac.meijo.android.s221205163;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijo.android.s221205163.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+        });
+
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
+
+
     }
 }
