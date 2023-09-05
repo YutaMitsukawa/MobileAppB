46aa21e4680c019bd67b94704d10d8b597e85f23
Yuta Mitsukawa <221205163@ccmailg.meijo-u.ac.jp>
2023-09-05 15:25:35 +0900

    DataStoreからデータを読み込む

--- app/src/main/java/jp/ac/meijo/android/s221205163/MainActivity.java
+++ app/src/main/java/jp/ac/meijo/android/s221205163/MainActivity.java
@@ -12,35 +12,57 @@ import jp.ac.meijo.android.s221205163.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
 
-        binding.button1.setOnClickListener(view -> {
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.text.setText(name));
+
+//        binding.saveButton.setOnClickListener(view -> {
+//            var text = binding.editTextText.getText().toString();
+//            binding.text.setText(text);
+//        });
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
+        binding.changeButton.setOnClickListener(view -> {
             var text = binding.editTextText.getText().toString();
             binding.text.setText(text);
         });
 
+//    protected void onStart() {
+//        super.onStart();
+//        prefDataStore.getString("name")
+//                .ifPresent(name -> binding.text.setText(name));
+//    }
 
-        binding.editTextText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                binding.text.setText(editable.toString());
-            }
-        });
+//        binding.editTextText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                binding.text.setText(editable.toString());
+//            }
+//        });
 
 
     }
